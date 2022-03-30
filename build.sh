fbac2501e9842d4ab9399e6b9d3f4b0d9bac8b0e
ronenk1 <ronenk1@rnd-hub.com>
2022-03-30 10:41:17 +0300

    fix(automation): v.0.0.3 + name fix

--- build.sh
+++ build.sh
@@ -16,19 +16,19 @@ IMAGE_FULL_NAME=cleanup-cli:$VERSION
 OUTPUT_DIR=/docker_images
 
 echo VERSION:$VERSION
-docker build --no-cache -t cleanup-cli:$VERSION --build-arg VERSION=$VERSION .
+docker build --no-cache -t automation-cleanup-cli:$VERSION --build-arg VERSION=$VERSION .
 
-docker tag cleanup-cli:$VERSION cleanup-cli:latest
+docker tag automation-cleanup-cli:$VERSION automation-cleanup-cli:latest
 
-echo cleanup-cli:$VERSION > ./docker_images/generated_dockers.txt
+echo automation-cleanup-cli:$VERSION > ./docker_images/generated_dockers.txt
 
 
 # path build.sh with "DUMP_IMAGE=1" to save docker
-FILE_NAME=cleanup-cli:${VERSION}.tar
+FILE_NAME=automation-cleanup-cli:${VERSION}.tar
 DUMP_OUTPUT_PATH=${OUTPUT_DIR}/releases/${FILE_NAME}
 
 if [[ ! -z "${DUMP_IMAGE}" ]]
 then
-     docker save -o ./${DUMP_OUTPUT_PATH} cleanup-cli:$VERSION
+     docker save -o ./${DUMP_OUTPUT_PATH} automation-cleanup-cli:$VERSION
      echo Saved docker image file into:${DUMP_OUTPUT_PATH}
 fi
--- generate_version.sh
+++ generate_version.sh
@@ -2,7 +2,7 @@
 pip3 install setuptools setuptools_scm
 VER=$(python3 setup.py --version | sed 's/+/./g')
 
-echo $(date):cleanup-cli:$VER
+echo $(date):automation-cleanup-cli:$VER
 
 echo version=$VER>version.txt
 echo version=$VER>>version_history.txt
